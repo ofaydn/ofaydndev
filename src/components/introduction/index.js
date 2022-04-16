518d14d103ccf1b70879f403f9845671277e12ed
Omer Faruk A <o.faduzce@gmail.com>
2022-04-17 02:41:05 +0300

    introduction changings

--- src/components/introduction/index.js
+++ src/components/introduction/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 const Intro = styled.div`
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;500&display=swap');
   margin: 0 auto;
   width:100%;
   padding: 3rem 1rem;
-  overflow: hidden;
-  
+  p{
+      font-size: 20px;
+      font-family: 'Montserrat', sans-serif;
+      font-weight: 500;
+  }
   `
 export default function App(props){
 return(
     <Intro>
-        <p>hey i'm Ömer Faruk Aydın
+        <p>hey i'm Ömer Faruk Aydın <br/>          
 i am a student who is working to be a software developer</p>
     </Intro>
     )
